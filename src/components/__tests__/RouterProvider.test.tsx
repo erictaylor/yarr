d67bb3ccc4be00ae26f738ee9045b65e1fa523d2
Eric Taylor <eric@daxos.com>
2021-08-24 21:36:48 -0600

    test: component RouterProvider

---
+++ src/components/__tests__/RouterProvider.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { RouterContext } from '../../context/RouterContext';
+import type { RouterContextProps } from '../../types';
+import { RouterProvider } from '../RouterProvider';
+import '@testing-library/jest-dom';
+
+describe('<RouterProvider />', () => {
+  it('should render children with access to router context', () => {
+    const router = 'mockedRouter' as unknown as RouterContextProps;
+
+    render(
+      <RouterProvider router={router}>
+        <RouterContext.Consumer>
+          {(value) => <div>{value}</div>}
+        </RouterContext.Consumer>
+      </RouterProvider>
+    );
+
+    expect(screen.getByText('mockedRouter')).toBeInTheDocument();
+  });
+});
