da45e8fa6dacea22d1424350599159353786cc8c
Eric Taylor <eric@daxos.com>
2021-08-28 10:57:16 -0600

    Revert "chore: update build cjs script to be async"
    This reverts commit cf01f2eb6f17bb0a29ce7b922097fa14883c43ae.

--- bin/build-cjs.js
+++ bin/build-cjs.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const esbuild = require('esbuild');
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 
 const sharedConfig = {
@@ -21,35 +21,24 @@ if (process.env.NODE_ENV === 'production') {
 }
 `;
 
-(async () => {
-  try {
-    await Promise.all([
-      esbuild.build({
-        ...sharedConfig,
-        outfile: './dist/cjs/yarr.cjs.production.min.js',
-        minify: true,
-      }),
+const main = () => {
+  esbuild.buildSync({
+    ...sharedConfig,
+    outfile: './dist/cjs/yarr.cjs.production.min.js',
+    minify: true,
+  });
 
-      esbuild.build({
-        ...sharedConfig,
-        outfile: './dist/cjs/yarr.cjs.development.js',
-      }),
-    ]);
-  } catch (error) {
-    console.log('An error occured when building the bundled cjs files');
-    throw error;
-  }
+  esbuild.buildSync({
+    ...sharedConfig,
+    outfile: './dist/cjs/yarr.cjs.development.js',
+  });
 
-  try {
-    await fs.writeFile(
-      path.resolve(__dirname, '../dist/cjs', 'index.js'),
-      indexContent
-    );
-  } catch (error) {
-    console.log('There was an error writing the cjs index file');
-    console.log(error);
-    process.exit(1);
-  }
+  fs.writeFileSync(
+    path.resolve(__dirname, '../dist/cjs', 'index.js'),
+    indexContent
+  );
 
   process.exit(0);
-})();
+};
+
+main();
